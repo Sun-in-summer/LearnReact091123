7adc1c9524bd6729913167fae060b8519c6b001a
Elena <eamatytsina@gmail.com>
2023-11-20 08:42:31 +0300

    mocks added

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,4 +1,7 @@
 import ReactDom from 'react-dom/client';
+import { restaurants } from './constants/mock';
 
 const root = ReactDom.createRoot(document.getElementById('root'));
-root.render('Hello');
+root.render(
+  
+);
