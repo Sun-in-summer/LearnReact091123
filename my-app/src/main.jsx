0f64097222feeae030682da931d3b036e8f1dd7d
Elena <eamatytsina@gmail.com>
2023-11-22 20:22:54 +0300

    2nd way of render is added

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,4 +1,5 @@
 import ReactDom from 'react-dom/client';
+// import React from 'react';
 import { restaurants } from './constants/mock';
 
 const root = ReactDom.createRoot(document.getElementById('root'));
@@ -62,3 +63,81 @@ root.render(
     </li>
   </ul>
 );
+
+// root.render(
+//   React.createElement("ul", {
+//     children: [
+//             React.createElement('li', {
+//         children:  [
+//           React.createElement('h2', {}, restaurants[0].name),
+//           React.createElement('h3', {}, "Menu" ),
+//           React.createElement('ul', {children: [
+//             React.createElement('li', {children: restaurants[0].menu[0].name}),
+//             React.createElement('li', {children: restaurants[0].menu[1].name}),
+//             React.createElement('li', {children: restaurants[0].menu[2].name}),
+//           ]}),
+//           React.createElement('h3', {}, "Reviews"),
+//           React.createElement('ul', {children: [
+//             React.createElement('li', {children: restaurants[0].reviews[0].text}),
+//             React.createElement('li', {children: restaurants[0].reviews[1].text}),
+//           ]}),
+//       ]
+//       }
+//       ),
+//             React.createElement('li', {
+//         children:  [
+//           React.createElement('h2', {}, restaurants[0].name),
+//           React.createElement('h3', {}, "Menu" ),
+//           React.createElement('ul', {children: [
+//             React.createElement('li', {children: restaurants[1].menu[0].name}),
+//             React.createElement('li', {children: restaurants[1].menu[1].name})
+
+//           ]}),
+//           React.createElement('h3', {}, "Reviews"),
+//           React.createElement('ul', {children: [
+//             React.createElement('li', {children: restaurants[1].reviews[0].text}),
+//             React.createElement('li', {children: restaurants[1].reviews[1].text}),
+//              React.createElement('li', {children: restaurants[1].reviews[2].text}),
+//           ]}),
+//       ]
+//       }
+//       ),
+//             React.createElement('li', {
+//         children:  [
+//           React.createElement('h2', {}, restaurants[0].name),
+//           React.createElement('h3', {}, "Menu" ),
+//           React.createElement('ul', {children: [
+//             React.createElement('li', {children: restaurants[2].menu[0].name}),
+//             React.createElement('li', {children: restaurants[2].menu[1].name}),
+//             React.createElement('li', {children: restaurants[2].menu[2].name}),
+//           ]}),
+//           React.createElement('h3', {}, "Reviews"),
+//           React.createElement('ul', {children: [
+//             React.createElement('li', {children: restaurants[2].reviews[0].text}),
+//           ]}),
+//       ]
+//       }
+//       ),
+//             React.createElement('li', {
+//         children:  [
+//           React.createElement('h2', {}, restaurants[0].name),
+//           React.createElement('h3', {}, "Menu" ),
+//           React.createElement('ul', {children: [
+//             React.createElement('li', {children: restaurants[3].menu[0].name}),
+//             React.createElement('li', {children: restaurants[3].menu[1].name})
+//           ]}),
+//           React.createElement('h3', {}, "Reviews"),
+//           React.createElement('ul', {children: [
+//             React.createElement('li', {children: restaurants[3].reviews[0].text}),
+//             React.createElement('li', {children: restaurants[3].reviews[1].text})
+//           ]}),
+//       ]
+//       }
+//       )
+//     ]
+
+//     }
+//   )
+// );
+
+
